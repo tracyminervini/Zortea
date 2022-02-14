46bfe3154bbb8db3531d79e1508dfccb41958a77
tracyminervini <tracy.dionisio@hotmail.com>
2022-02-14 11:40:32 -0400

    Componentes

---
+++ src/components/FormInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <label :class="labelClass">{{ label }}</label>
+  <input :type="type" :class="inputClass" :value="value" @input="$emit('input', $event.target.value)" />
+</template>
+
+<script>
+
+export default {
+  props: {
+    type: {
+      type: String,
+      require: true,
+      default: "text",
+    },
+    label: {
+      type: String,
+      require: true,
+      default: "",
+    },
+    labelClass: {
+      type: String,
+      require: true,
+      default: "",
+    },
+    inputClass: {
+      type: String,
+      require: true,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      require: false,
+      default: ""
+    },
+    value: {
+      type: String,
+      default: "",
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
