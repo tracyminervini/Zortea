06d457acfe13d93ac41fa936b707e785e0f7dc84
tracyminervini <tracy.dionisio@hotmail.com>
2022-02-14 11:40:18 -0400

    Aplicação

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Navbar :logo="logo_src" :alt="app_name" />
+  <router-view />
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Cadastro de Fornecedores",
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  font-family: "Oxygen", sans-serif;
+  background-color: f2f2f2;
+}
+</style>
